5ec0abd2b8c406eb46545f601798571a91dfa2a9
Anna Krasnovid <krasnovidanka@yandex.ru>
2022-05-12 10:32:26 +0300

    bugfix

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -4,13 +4,13 @@ function SearchForm() {
   return (
     <section className="search">
       <div className="search__container">
-        <div className="search__box">
+        <form className="search__box">
           <div className="search__box-search">
             <div className="search__magnifier"></div>
-            <input className="search__input" type="text" placeholder="Фильм"></input>
+            <input className="search__input" type="text" placeholder="Фильм" required></input>
           </div>
           <button className="search__button hover-button" type="submit"></button>
-        </div>
+        </form>
         <div className="search__box-checkbox">
           <label className="search__checkbox">
             <input className="search__checkbox-input" type="checkbox"></input>
